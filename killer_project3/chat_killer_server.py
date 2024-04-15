8b31eb30070e3816c71a3195a65aa2e27155e18f
TerminalGambit <jackmassey.monaco@outlook.com>
2024-04-15 16:14:45 +0200

    Created the chat_killer_server.py file and added the consigne to it

---
+++ killer_project3/chat_killer_server.py
@@ -0,0 +1,8 @@
+# chat_killer_server.py : l'exécutable lancé par le modérateur de jeu. 
+# Cet exécutable lance le serveur de chat dans le terminal (reste attaché au terminal). 
+# Le modérateur, via cet exécutable:
+# • peut suivre toutes les discussions
+# • connait tous les secrets
+# • decide quand lancer la partie
+# • peut suspendre temporairement ou bannir définitivement un joueur au cours de partie (c'est un modérateur)
+# • vérifie que le programmeur a bien fait son travail (débogage)
