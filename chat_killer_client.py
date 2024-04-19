64f8cadd153a0100c887428e43b06ab069db7b4d
TerminalGambit <jackmassey.monaco@outlook.com>
2024-04-19 12:05:32 +0200

    added the code from the videos I saw, I hope it doesn't bungle our repo and the sync :)

--- chat_killer_client.py
+++ chat_killer_client.py
@@ -4,3 +4,24 @@
 # Les commandes commencent par un !, les messages privés commencent par @toto (pour envoyer à toto), 
 # ou @toto @titi @tata pour envoyer un message privé à plusieurs destinataires. Lorsque le joueur meurt, 
 # toutes les fenêtres et les processus créés pour lui doivent être tués
+
+import socket
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) # get the IP address of the machine
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR) # connect to the server's address and port number
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
--- chat_killer_server.py
+++ chat_killer_server.py
@@ -6,3 +6,48 @@
 # • decide quand lancer la partie
 # • peut suspendre temporairement ou bannir définitivement un joueur au cours de partie (c'est un modérateur)
 # • vérifie que le programmeur a bien fait son travail (débogage)
+
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) # get the IP address of the machine
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+# AF_INET is the address family for IPv4, and SOCK_STREAM is the socket type for TCP
+
+server.bind(ADDR) # The address is a tuple containing the hostname and port number
+# essentially, this is the server's address and port number that the server will listen on
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) # receive the message length
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT) # receive the message
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+            conn.send("Message received".encode(FORMAT)) # send a message back to the client
+
+    conn.close()
+
+def start():
+    server.listen() # The backlog parameter specifies the maximum number of queued connections
+    print(f"[LISTENING] server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept() # Returns a new socket object and the address of the client
+        # handle_client(conn, addr)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] server is starting...")
+start()
