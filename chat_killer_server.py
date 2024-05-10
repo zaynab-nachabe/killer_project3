cb705e1368233080645b38edc1a27c0269111db0
TerminalGambit <jackmassey.monaco@outlook.com>
2024-05-10 20:48:19 +0200

    few adjustments

--- chat_killer_server.py
+++ chat_killer_server.py
@@ -128,9 +128,10 @@ def gestion_message(connection, client_address, server_socket):
                 else:
                     print('reconnect test here 2')
                     for client_socket, val in clients_dict.items():
+                        conn, addr = client_socket
                         if client_socket != server_socket and client_socket != connection:
                             if val[1] == "connected":  # Check if client is still connected
-                                client_socket[0].sendall(f"{pseudo}: {client_message}\n".encode())
+                                conn.sendall(f"{pseudo}: {client_message}\n".encode())
         else:
             pass
     except Exception as error:
